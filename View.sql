5d305a70dbe085a92aa90af2eefb2e8b9ba1c46c
Aryan Pandit <aryanclovis20@gmail.com>
2025-03-16 21:51:43 -0400

    pushing view 2

--- View.sql
+++ View.sql
@@ -9,3 +9,11 @@ LEFT JOIN Booking b ON r.Room_Number = b.Room_Number
     AND r.H_Building_No = b.H_Building_No
 LEFT JOIN Renting rt ON b.b_ID = rt.b_ID  -- Only count fully rented bookings
 GROUP BY h.Address_City;
+
+-- View 2
+CREATE VIEW Aggregated_Capacity_Per_Hotel AS
+SELECT h.H_Building_No, h.c_Name, h.Address_City, SUM(r.capacity) AS total_capacity
+FROM Hotel h
+JOIN Room r ON h.H_Building_No = r.H_Building_No
+GROUP BY h.H_Building_No, h.c_Name, h.Address_City
+ORDER BY h.H_Building_No ASC;
